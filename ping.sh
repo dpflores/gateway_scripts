0c28dece4fd5811bfcde5cab28e5a7cc538149c1
acme <acme@acmecia.com>
2024-04-09 12:19:23 -0500

    u

---
+++ ping.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Verifica si se han proporcionado la cantidad correcta de argumentos
+if [ "$#" -ne 2 ]; then
+    echo "Uso: $0 <servidor> <intervalo_en_segundos>"
+    exit 1
+fi
+
+server="$1"
+interval="$2"
+
+# Verifica si el intervalo es un número válido
+if ! [[ "$interval" =~ ^[0-9]+$ ]]; then
+    echo "El intervalo debe ser un número entero válido."
+    exit 1
+fi
+
+# Bucle infinito que ejecuta el ping cada intervalo de tiempo
+while true; do
+    ping -c 1 $server > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "1"
+    fi
+    sleep "$interval"
+done
